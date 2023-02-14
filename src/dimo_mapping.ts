070c0947f462b4103d0f75e27c61f95040f51509
Arthur <32548365+Arthh@users.noreply.github.com>
2023-02-14 12:59:20 -0300

    feat: delegateChange entity and delegatinHistory Entity

--- src/dimo_mapping.ts
+++ src/dimo_mapping.ts
@@ -3,13 +3,18 @@ import {
   User,
   DelegateOrganization,
   DelegatorOrganization,
+  DelegatingHistory,
+  DelegateChange,
+  DelegateVotingPowerChange,
 } from "../generated/schema";
 import {
   DelegateChanged,
   DelegateVotesChanged,
+  Transfer,
 } from "../generated/DimoToken/DimoToken";
 import { getDelegateOrganization } from "./shared/getDelegateOrganization";
 import { getFirstTokenDelegatedAt } from "./shared/getFirstTokenDelegatedAt";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function delegateChanged(event: DelegateChanged): void {
   let organization = new Organization("dimo");
@@ -28,7 +33,31 @@ export function delegateChanged(event: DelegateChanged): void {
   delegatorOrganization.delegate = delegate.id;
   delegatorOrganization.delegator = delegator.id;
   delegatorOrganization.organization = organization.id;
+
+  
+  let delegatingHistory = DelegatingHistory.load(event.transaction.hash.toHexString())
+
+  if(!delegatingHistory){
+    delegatingHistory = new DelegatingHistory(event.transaction.hash.toHexString())
+    delegatingHistory.amount = BigInt.zero();
+    delegatingHistory.timestamp = event.block.timestamp;
+  }
+
+  delegatingHistory.fromDelegate = event.params.fromDelegate.toHexString();
+  delegatingHistory.toDelegate = event.params.toDelegate.toHexString();
+  delegatingHistory.delegator = delegator.id;
+
+  delegatingHistory.save();
   delegatorOrganization.save();
+
+  const delegateChange = new DelegateChange(event.transaction.hash.toHexString());
+  delegateChange.oldDelegate = event.params.fromDelegate.toHexString(),
+  delegateChange.newDelegate = event.params.toDelegate.toHexString(),
+  delegateChange.delegator = event.params.delegator.toHexString(),
+  delegateChange.blockTimestamp = event.block.timestamp,
+  delegateChange.txnHash = event.transaction.hash.toHexString(),
+  delegateChange.blockNumber = event.block.number,
+  delegateChange.save();
 }
 
 export function delegateVotesChanged(event: DelegateVotesChanged): void {
@@ -52,4 +81,55 @@ export function delegateVotesChanged(event: DelegateVotesChanged): void {
   );
 
   delegateOrganization.save();
+  
+  
+  let delegatingHistory = DelegatingHistory.load(event.transaction.hash.toHexString())
+
+  if(!delegatingHistory){
+    delegatingHistory = new DelegatingHistory(event.transaction.hash.toHexString())
+    delegatingHistory.amount = BigInt.zero();
+    delegatingHistory.timestamp = event.block.timestamp;
+    delegatingHistory.delegator = "0";
+  }
+
+  let delegatedAmount = event.params.newBalance.minus(event.params.previousBalance)
+
+  if (delegatedAmount < BigInt.zero()) {
+    delegatingHistory.fromDelegate = user.id
+  } else {
+    delegatingHistory.toDelegate = user.id
+  }
+  if (delegatingHistory.amount) {
+    delegatingHistory.amount = delegatingHistory.amount.plus(delegatedAmount);
+  } else {
+    delegatingHistory.amount = delegatedAmount;
+  }
+
+  delegatingHistory.save();
+
+  const delegatePowerChange = new DelegateVotingPowerChange(
+    event.transaction.hash.toHexString()
+  );
+
+  delegatePowerChange.previousBalance = event.params.previousBalance;
+  delegatePowerChange.newBalance = event.params.newBalance;
+  delegatePowerChange.delegate = event.params.delegate.toHexString();
+  delegatePowerChange.tokenAddress = event.address.toHexString();
+  delegatePowerChange.txnHash = event.transaction.hash.toHexString();
+  delegatePowerChange.blockTimestamp = event.block.timestamp;
+  delegatePowerChange.blockNumber = event.block.number;
+  delegatePowerChange.save();
+}
+
+
+export function transfer(event: Transfer): void {
+  let delegatingHistory = DelegatingHistory.load(event.transaction.hash.toHexString())
+  if(!delegatingHistory){
+    delegatingHistory = new DelegatingHistory(event.transaction.hash.toHexString())
+    delegatingHistory.amount = BigInt.zero();
+    delegatingHistory.timestamp = event.block.timestamp;
+    delegatingHistory.delegator = event.params.from.toHexString();
+  }
+  delegatingHistory.timestamp = event.block.timestamp;
+  delegatingHistory.save();
 }
